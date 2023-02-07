4b3cd1a372743be5dd841797a472870b2ea4065c
Mehdi <mehdi.arroussi@ynov.com>
2023-02-07 05:11:02 +0100

    Bug php

--- home.php
+++ home.php
@@ -8,21 +8,21 @@
 
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+    </head>
 
-<body>
-    <?php
-    if ($identity== null): ?>
-    <a href="#" > Click here to Login/Signin </a>
+    <body>
+       
+        <a href="./static/login.php" > Click here to Login/Signin </a>
 
-    <?php else:?>
-<h1> Bienvenue </h1>
+        
+    <h1> Bienvenue </h1>
 
 
-</body>
+
+    </body>
 </html>
